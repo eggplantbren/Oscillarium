dced86f60c0123fb5433708fd659f4b325943713
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-30 19:15:57 +1200

    Basic prior

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXX = g++
 CXXFLAGS = -O3 -march=native -Wall -Wextra -pedantic
-INCLUDE = -I $(DNEST4_PATH)
+INCLUDE = -I $(DNEST4_PATH) -I celerite2/c++/include -I /usr/include/eigen3
 
 default:
 	$(CXX) $(CXXFLAGS) $(INCLUDE) -c Data.cpp
--- MyModel.cpp
+++ MyModel.cpp
@@ -1,5 +1,6 @@
 #include "MyModel.h"
 #include "DNest4/code/DNest4.h"
+#include <celerite2/terms.hpp>
 
 namespace Oscillarium
 {
@@ -11,29 +12,52 @@ MyModel::MyModel()
 
 void MyModel::from_prior(DNest4::RNG& rng)
 {
-
+    a = 100.0*rng.rand();
+    f = 100.0*rng.rand();
+    q = 0.5 + 99.5*rng.rand();
 }
 
 double MyModel::perturb(DNest4::RNG& rng)
 {
     double logH = 0.0;
+
+    int which = rng.rand_int(3);
+    if(which == 0)
+    {
+        a += 100.0*rng.randh();
+        DNest4::wrap(a, 0.0, 100.0);
+    }
+    else if(which == 1)
+    {
+        f += 100.0*rng.randh();
+        DNest4::wrap(f, 0.0, 100.0);
+    }
+    else
+    {
+        q += 99.5*rng.randh();
+        DNest4::wrap(q, 0.5, 100.0);
+    }
+
     return logH;
 }
 
 double MyModel::log_likelihood() const
 {
     double logL = 0.0;
+
+
+
     return logL;
 }
 
 void MyModel::print(std::ostream& out) const
 {
-
+    out << a << ' ' << f << ' ' << q;
 }
 
 std::string MyModel::description() const
 {
-    return std::string("");
+    return std::string("a f q");
 }
 
 } // namespace
